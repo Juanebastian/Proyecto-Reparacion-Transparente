c5613cbcd00deb8a1da21ce9d1c1aa125f7b6844
JUAN SEBASTIAN DIAZ CHAPARRO <juansebastiandiazchaparro27@gmail.com>
2025-04-30 20:35:59 -0500

    actualizacion faltante basada en  deppscan

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,6 +1,5 @@
 import { Controller, Get,  Body, Param, Delete, Put, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { AuthGuard } from '@nestjs/passport';
 import { AdminGuard } from 'src/auth/admin.guard';
